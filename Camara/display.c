ed1b053ae0d7ad882244b9742d60e388b4fceff5
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-12-07 19:45:51 +0100

    Poniendo en orden todo
    la verdad que no sé qué hice la última vez

--- Camara/display.c
+++ Camara/display.c
@@ -2,6 +2,7 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <stdio.h>
 
 /** EXTERNAL VARIABLES **/
 
@@ -12,6 +13,70 @@ extern GLdouble _ortho_z_min,_ortho_z_max;
 
 extern object3d *_first_object;
 extern object3d *_selected_object;
+extern camara *_first_camara;
+extern camara *_selected_camara;
+extern int visualizar;         
+extern int proyeccion;                  // perspectiva=0; paralela =1;
+extern int referencia ; // local = 0; global = 1 
+
+
+/**
+ * @brief Function to calculate de matrix Mcsr of the camera
+ */
+void obtenerMCSR(GLdouble *M, GLdouble *MCSR){
+    MCSR[0]=M[0];
+    MCSR[1]=M[4];
+    MCSR[2]=M[8];
+    MCSR[3]=0;
+    MCSR[4]=M[1];
+    MCSR[5]=M[5];
+    MCSR[6]=M[9];
+    MCSR[7]=0;
+    MCSR[8]=M[2];
+    MCSR[9]=M[6];
+    MCSR[10]=M[10];
+    MCSR[11]=0;
+    MCSR[12]=-(M[0]*M[12]+M[1]*M[13]+M[2]*M[14]);
+    MCSR[13]=-(M[4]*M[12]+M[5]*M[13]+M[6]*M[14]);
+    MCSR[14]=-(M[8]*M[12]+M[9]*M[13]+M[10]*M[14]);
+    MCSR[15]=1;
+}
+
+
+
+int poligono_delantero(object3d *o,face f){
+	/*First we get the camera location*/
+	vector3 e;
+	e.x = _selected_camara->M[12]; 
+	e.y = _selected_camara->M[13];
+	e.z = _selected_camara->M[14];
+	
+	/*Then, we pass the camera location to the object reference system*/
+	GLdouble csr_objeto[16];
+	obtenerMCSR(_selected_object->mptr->M,csr_objeto);
+	e.x = e.x*csr_objeto[0] + e.y*csr_objeto[1] + e.z*csr_objeto[2];
+	e.y = e.x*csr_objeto[4] + e.y*csr_objeto[5] + e.z*csr_objeto[6];
+	e.z = e.x*csr_objeto[8] + e.y*csr_objeto[9] + e.z*csr_objeto[10];
+	
+	/*Now we calculate the vector between the camera and the face*/
+	
+	vector3 v;
+	vertex p_obj = o->vertex_table[f.vertex_table[0]];
+	v.x = p_obj.coord.x - e.x;
+	v.y = p_obj.coord.y - e.y;
+	v.z = p_obj.coord.z - e.z;
+	
+	/*Finally we calculate the escalar product of the normal and the v vector to see the angle between them*/
+	
+	GLdouble escalar = v.x * f.normal.x + v.y * f.normal.y + v.z * f.normal.z;
+	
+	if(escalar < 0){
+		return 0;
+	}
+	else{
+		return 1;
+	}
+}
 
 /**
  * @brief Function to draw the axes
@@ -51,13 +116,14 @@ void reshape(int width, int height) {
     _window_ratio = (GLdouble) width / (GLdouble) height;
 }
 
-
 /**
  * @brief Callback display function
  */
 void display(void) {
     GLint v_index, v, f;
     object3d *aux_obj = _first_object;
+    camara *aux_camara = _first_camara;
+    GLdouble MCSR[16];
 
     /* Clear the screen */
     glClear(GL_COLOR_BUFFER_BIT);
@@ -66,33 +132,38 @@ void display(void) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    /*When the window is wider than our original projection plane we extend the plane in the X axis*/
-    if ((_ortho_x_max - _ortho_x_min) / (_ortho_y_max - _ortho_y_min) < _window_ratio) {
-        /* New width */
-        GLdouble wd = (_ortho_y_max - _ortho_y_min) * _window_ratio;
-        /* Midpoint in the X axis */
-        GLdouble midpt = (_ortho_x_min + _ortho_x_max) / 2;
-        /*Definition of the projection*/
-        glOrtho(midpt - (wd / 2), midpt + (wd / 2), _ortho_y_min, _ortho_y_max, _ortho_z_min, _ortho_z_max);
-    } else {/* In the opposite situation we extend the Y axis */
-        /* New height */
-        GLdouble he = (_ortho_x_max - _ortho_x_min) / _window_ratio;
-        /* Midpoint in the Y axis */
-        GLdouble midpt = (_ortho_y_min + _ortho_y_max) / 2;
-        /*Definition of the projection*/
-        glOrtho(_ortho_x_min, _ortho_x_max, midpt - (he / 2), midpt + (he / 2), _ortho_z_min, _ortho_z_max);
+ 
+    if( proyeccion== 0) { // perspectiva
+        glFrustum(-0.1, 0.1, -0.1, 0.1, 0.1, 100.0);
     }
+    else{ // paralela
+        glOrtho(-5.0, 5.0, -5.0, 5.0, 0.0, 100.0);
+    }
+
+
+    
+
+    if((_selected_object!=NULL) && (visualizar==0)){
+        obtenerMCSR(_selected_object->mptr->M, MCSR);
+    }
+    if ((_selected_object!=NULL) && (_selected_camara!=NULL) && (visualizar==1)){
+        obtenerMCSR(_selected_camara->M, MCSR);
+    }
+
 
     /* Now we start drawing the object */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-
+    //glLoadMatrixd(MCSR);
+    
     /*First, we draw the axes*/
     draw_axes();
 
+   
+    
     /*Now each of the objects in the list*/
-    while (aux_obj != 0) {
-
+    while(aux_obj != 0) {
+        
         /* Select the color, depending on whether the current object is the selected one or not */
         if (aux_obj == _selected_object){
             glColor3f(KG_COL_SELECTED_R,KG_COL_SELECTED_G,KG_COL_SELECTED_B);
@@ -100,9 +171,15 @@ void display(void) {
             glColor3f(KG_COL_NONSELECTED_R,KG_COL_NONSELECTED_G,KG_COL_NONSELECTED_B);
         }
 
+
         /* Draw the object; for each face create a new polygon with the corresponding vertices */
-        //glLoadIdentity();
-        glLoadMatrixd(aux_obj->mptr->M);
+  
+        glLoadMatrixd(MCSR);
+        glMultMatrixd(aux_obj->mptr->M);
+        // dibujar los objetos
+        
+        // es_visible(object3d *optr, aux_obj->num_faces, punto3 *pptr)
+        
         for (f = 0; f < aux_obj->num_faces; f++) {
             glBegin(GL_POLYGON);
             for (v = 0; v < aux_obj->face_table[f].num_vertices; v++) {
@@ -110,11 +187,10 @@ void display(void) {
                 glVertex3d(aux_obj->vertex_table[v_index].coord.x,
                         aux_obj->vertex_table[v_index].coord.y,
                         aux_obj->vertex_table[v_index].coord.z);
-
             }
             glEnd();
         }
-        aux_obj = aux_obj->next;
+        aux_obj = aux_obj->next; 
     }
     /*Do the actual drawing*/
     glFlush();
