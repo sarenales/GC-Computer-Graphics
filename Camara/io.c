f0cbd5de82bd376fdf6735fc5c7e90fc52fd88eb
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-12-09 15:19:27 +0100

    He movido las funciones relacionadas a las cámaras
    Para hacerlo un poco más modular y que no sea un caos.

--- Camara/io.c
+++ Camara/io.c
@@ -1,7 +1,12 @@
-#include "definitions.h"
-#include "load_obj.h"
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "definitions.h"
+#include "load_obj.h"
+#include "camara.h"
 
 extern object3d * _first_object;
 extern object3d * _selected_object;
@@ -21,55 +26,7 @@ extern int vista;
 int movimiento = 1; // rota = 0 ; trasladar = 1; escalar = 2;
 
 int i;
-float E, at, Vup;   // E = posicion de la camara; 
-                    // at = punto donde mira la camara; 
-                    // Vvp = vector verticalidad
 
-
-
-void get_matriz_objeto(GLdouble* m1, vector3* e,GLdouble* m2){
-    for(int i = 0; i < 3; i++){
-    	for (int j = 0; j < 3;j++){
-    			m2[i+4*j] = m1[4*i+j];
-    	}
-    }
-    m2[3] = 0;
-    m2[7] = 0;
-    m2[11] = 0;
-    m2[12] = e->x;
-    m2[13] = e->y;
-    m2[14] = e->z;
-    m2[15] = 1;
-}
-void cargar_obj_camara(camara *cam){
-    object3d auxiliar_object;
-    read_wavefront("camara.obj", &auxiliar_object);
-
-    /*There is not error treatment because camara.obj will always be there*/
-
-    cam->face_table = auxiliar_object.face_table;
-    cam->num_faces = auxiliar_object.num_faces;
-    cam->vertex_table = auxiliar_object.vertex_table;
-    cam->num_vertices = auxiliar_object.num_vertices;
-    cam->max = auxiliar_object.max;
-    cam->min = auxiliar_object.min;
-
-}
-
-void mirar_obj_selec(){
-		vector3 obj,cam;
-		obj.x = _selected_object->mptr->M[12];
-		obj.y = _selected_object->mptr->M[13];
-		obj.z = _selected_object->mptr->M[14];
-        
-		cam.x = _selected_camara->M[12];
-		cam.y = _selected_camara->M[13];
-		cam.z = _selected_camara->M[14];
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		gluLookAt(cam.x,cam.y,cam.z,obj.x,obj.y,obj.z,0,1,0);
-		glGetDoublev(GL_MODELVIEW_MATRIX,_selected_camara->M);
-}
 
 
 /**
@@ -190,8 +147,8 @@ void keyboard(unsigned char key, int x, int y) {
             glGetDoublev(GL_MODELVIEW_MATRIX,m1->M);
             get_matriz_objeto(m1->M,e,m2->M);
 
-            auxiliar_camara->M = m1->M;
-            auxiliar_camara->M = m2->M;
+           // auxiliar_camara->M = m1->M;
+            // auxiliar_camara->M = m2->M;
             
             auxiliar_camara->projection[0] = -0.1;
             auxiliar_camara->projection[1] = 0.1;
@@ -467,7 +424,8 @@ void keyboard(unsigned char key, int x, int y) {
                     modo = CAMARA;
                     printf("Modo Camara. \n");
                 }
-            }
+            }else 
+                printf("No hay ningun objeto cargado. \n");
         break;
         
         
