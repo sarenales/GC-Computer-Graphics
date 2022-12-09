b48dbd94c4e12a3f2833a5de492f9998b78e740e
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-12-09 15:21:48 +0100

    Create camara.h

---
+++ Camara/camara.h
@@ -0,0 +1,7 @@
+#include <GL/glu.h>
+#include "definitions.h"
+
+void get_matriz_objeto(GLdouble* m1, vector3* e,GLdouble* m2);
+void cargar_obj_camara(camara *cam);
+void mirar_obj_selec();
+void obtenerMCSR(GLdouble *M, GLdouble *MCSR);
