9aab363124bdef4a2d3664d64b563c826c9d0586
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-12-15 09:43:36 +0100

    Update camara.h

--- Camara/camara.h
+++ Camara/camara.h
@@ -3,5 +3,9 @@
 
 void get_matriz_objeto(GLdouble* m1, vector3* e,GLdouble* m2);
 void cargar_obj_camara(camara *cam);
+void add_camara();
+void add_camara_to_list(camara *cam);
+void create_camara(vector3 pos_camara, vector3 front_cam, vector3 up_cam,camara *c);
+void centre_camera_to_obj(object3d *obj);
 void mirar_obj_selec();
-void obtenerMCSR(GLdouble *M, GLdouble *MCSR);
+void inicial_camaras();
