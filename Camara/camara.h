e8b429716a06182ff5033f2a7ac1dfc1b481c44b
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-12-30 19:29:06 +0100

    siento q pierdo el tiempo pa luego sacar un cerapio

--- Camara/camara.h
+++ Camara/camara.h
@@ -1,11 +1,11 @@
 #include <GL/glu.h>
 #include "definitions.h"
 
-void get_matriz_objeto(GLdouble* m1, vector3* e,GLdouble* m2);
-void cargar_obj_camara(camara *cam);
-void add_camara();
-void add_camara_to_list(camara *cam);
-void create_camara(vector3 pos_camara, vector3 front_cam, vector3 up_cam,camara *c);
+void default_cameras();
+void add_camara_mode_obj(object3d *obj);
+void matriz_inversa(camara *c);
+void modo_analisis(int x, int y);
 void centre_camera_to_obj(object3d *obj);
-void mirar_obj_selec();
-void inicial_camaras();
+void create_camara(vector3 pos_camara, vector3 front_cam, vector3 up_cam,camara *c);
+void add_camara_to_list(camara *cam);
+void add_camara();
