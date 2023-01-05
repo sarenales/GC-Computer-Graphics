09b7bc0a4d49f02757ea9aa61bd4a67d7c3dfe96
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2023-01-05 09:23:15 +0100

    Create iluminacion.c

---
+++ Iluminacion/iluminacion.c
@@ -0,0 +1,10 @@
+#include <GL/glut.h>
+#include "definitions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "io.h"
+
+extern object3d *_selected_object;
+extern object3d *cam_object;
+extern camara * _selected_camara;
