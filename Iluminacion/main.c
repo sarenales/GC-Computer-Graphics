dcbd476b8985034c8ee4d166d19fcfdf8ab091ce
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2023-01-05 09:21:47 +0100

    Update main.c

--- Iluminacion/main.c
+++ Iluminacion/main.c
@@ -16,21 +16,29 @@ GLdouble _ortho_x_min,_ortho_x_max;         /*Variables for the control of the o
 GLdouble _ortho_y_min ,_ortho_y_max;        /*Variables for the control of the orthographic projection*/
 GLdouble _ortho_z_min,_ortho_z_max;         /*Variables for the control of the orthographic projection*/
 
-object3d * _first_object= 0;                /*List of objects*/
+object3d * _first_object = 0;               /*List of objects*/
 object3d * _selected_object = 0;            /*Object currently selected*/
-camara *_first_camara=0;
-camara *_selected_camara=0;
+object3d * _camara_objeto = 0;
+object3d * light_object = 0;
 
-elemM *m1=0;
-elemM *m2=0;
+camara *_first_camara = 0;
+camara *_selected_camara = 0;
+camara *_object_camara = 0;
 
+elemM *m1 = 0;
+elemM *m2 = 0;
 
-int referencia = 0;                         /*global, local --> para obj*/
-int modo = 0;                               /*obj, cam, camaraobj, iluminacion*/
+//objetos_luz luces_globales[8];
 
+int _selected_light = 0; 
 
-int proyeccion=0;                           /* perspectiva, paralela*/
-int vista=0;                                /* vuelo, analisis --> para cam*/
+int referencia = GLOBAL;                    /*global, local --> para obj*/
+int modo = OBJETO;                          /*obj, cam, camaraobj, iluminacion*/
+int movimiento = TRASLADAR; 
+int proyeccion = PERSPECTIVA;               /* perspectiva, paralela*/
+int vista = VUELO;                          /* vuelo, analisis --> para cam*/
+
+int luz = ON;
 
 
 
@@ -49,56 +57,16 @@ void initialization (){
     _window_ratio = (GLdouble) KG_WINDOW_WIDTH / (GLdouble) KG_WINDOW_HEIGHT;
 
     /*Definition of the background color*/
-    glClearColor(KG_COL_BACK_R, KG_COL_BACK_G, KG_COL_BACK_B, KG_COL_BACK_A);
-
-    _selected_camara =(camara*) malloc(sizeof(camara));
+//    glClearColor(KG_COL_BACK_R, KG_COL_BACK_G, KG_COL_BACK_B, KG_COL_BACK_A);
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    // _selected_camara =(camara*) malloc(sizeof(camara));
     
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-     glGetDoublev(GL_MODELVIEW_MATRIX,_selected_camara->M);
-     _first_camara=_selected_camara;
-    
-    
-    // _first_camara =(camara*) malloc(sizeof(camara));
-    // printf("MAIN llega aqui 1\n");
-   
-    // vector3* a;
-    // a->x = 5;
-    // a->y = 5;
-    // a->z = 5;
-    
-    // m1 =(elemM*) malloc( sizeof(elemM));
-    // m2 =(elemM*) malloc( sizeof(elemM));
-
-    // printf("MAIN llega aqui 2\n");
-    
-    
-    
-    //printf("llega aqui 2.1\n");
+    // glMatrixMode(GL_MODELVIEW);
     // glLoadIdentity();
-    // printf("MAIN no le gusta el glulookat \n");
-    // gluLookAt(a->x,a->y,a->z,0,0,0,0,1,0);
-    // gluLookAt(5,5,5,0,0,0,0,1,0);
-    // printf("MAIN llega aqui 3:  le gusta el glulookat \n");
-    
     // glGetDoublev(GL_MODELVIEW_MATRIX,_selected_camara->M);
+    // _selected_camara->M[14] = 10;
     // _first_camara=_selected_camara;
-    // printf("MAIN llega aqui 4\n");
-    
-    // get_matriz_objeto(m1->M, a, m2->M);
-    // printf("MAIN llega aqui 5\n");
-    
-    // _selected_camara->projection[0] = -0.1;
-    // _selected_camara->projection[1] = 0.1;
-    // _selected_camara->projection[2] = -0.1;
-    // _selected_camara->projection[3] = 0.1;
-    // _selected_camara->projection[4] = 0.1;
-    // _selected_camara->projection[5] = 1000;
-    // printf("MAIN llega aqui 6\n");
-    
-    // cargar_obj_camara(_selected_camara);
-    // _selected_camara->next = 0;
-    // _selected_camara=_first_camara;
+    // inicial_camaras(_selected_camara);
     
     /*Definition of the method to draw the objects*/
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
@@ -121,6 +89,18 @@ int main(int argc, char** argv) {
     glutInitWindowPosition(KG_WINDOW_X, KG_WINDOW_Y);
     glutCreateWindow(KG_WINDOW_TITLE);
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // queremos poligonos opacos
+    
+    glEnable(GL_DEPTH_TEST); // activar el test de profundidad (Z-buffer)
+
+    
+    // if (flat_smooth) 
+        // glShadeModel(GL_SMOOTH);  // hacen falta los vectores normales de cada vertice
+    // else 
+        // glShadeModel(GL_FLAT);  // basta con vector normal del poligono
+    
+
+
     /* set the callback functions */
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
@@ -129,6 +109,8 @@ int main(int argc, char** argv) {
 
     /* this initialization has to be AFTER the creation of the window */
     initialization();
+    load_presentation();
+    default_cameras();
 
     /* start the main loop */
     glutMainLoop();
